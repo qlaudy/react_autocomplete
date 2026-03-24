5f043a526c96cea626d086a876ba3ec9861195c2
unknown <dkuz6323@gmail.com>
2026-03-24 18:27:12 +0200

    completed tasks

---
+++ src/Autocomplete.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+import { Person } from './types/Person';
+
+interface Props {
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+}
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  onSelected,
+  delay = 300,
+}) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppleidQuery] = useState(query);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (query === appliedQuery) {
+      return;
+    }
+
+    const handler = setTimeout(() => {
+      setAppleidQuery(query);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [query, delay]);
+
+  const filteredPeople = people.filter(person =>
+    person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+  );
+
+  return (
+    <>
+      <div className={`dropdown ${isOpen ? 'is-active' : ''}`}>
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onChange={event => {
+              const value = event.target.value;
+
+              setQuery(value);
+              onSelected(null);
+            }}
+            onFocus={() => setIsOpen(true)}
+          />
+        </div>
+
+        {isOpen && filteredPeople.length > 0 && (
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+            <div className="dropdown-content">
+              {filteredPeople.map(person => (
+                <div
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  key={person.slug}
+                  onClick={() => {
+                    onSelected(person);
+                    setQuery(person.name);
+                    setAppleidQuery(person.name);
+                    setIsOpen(false);
+                  }}
+                >
+                  <p
+                    className={
+                      person.sex === 'f' ? 'has-text-danger' : 'has-text-link'
+                    }
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      {isOpen && appliedQuery && filteredPeople.length === 0 && (
+        <div
+          className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
